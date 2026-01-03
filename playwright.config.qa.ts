a037718a3f1ad03c8f7fdf55c4b7d840ff20c9a4
pavan-chaganti <saraswathipavankumarc@gmail.com>
2026-01-03 13:37:25 +0530

    updated config files

--- playwright.config.qa.ts
+++ playwright.config.qa.ts
@@ -46,7 +46,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: true,
+    headless: !! process.env.CI,
     screenshot:"only-on-failure",
     video:"off",
     baseURL: "https://naveenautomationlabs.com/opencart/index.php",
